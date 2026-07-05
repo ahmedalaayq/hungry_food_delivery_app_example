a611705d6d83fdbe2293d0d28b3efd7210eeebbb
ahmedalaayq <ahmed.alaayq@gmail.com>
2026-07-06 00:44:31 +0300

    create splash controller

--- lib/features/splash/controller/splash_controller.dart
+++ lib/features/splash/controller/splash_controller.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hungry/core/dataSources/local/preference_manager.dart';
+import 'package:hungry/features/auth/login/views/login_view.dart';
 import 'package:hungry/root.dart';
 
 class SplashController {
   Future<void> excuteLogic(BuildContext context) async {
     await Future.delayed(Duration(seconds: 2));
     if (!context.mounted) return;
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const Root()),
-    );
+
+    final bool isLoggedIn =
+        PreferenceManager.getData<bool>("isLoggedIn") ?? false;
+
+        if(isLoggedIn){
+Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Root()),
+      );
+        }else {
+          Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginView()),
+      );
+        }
+    
   }
 }
